21286f6fbe1d8458ddb1a2c04d199edaa9bb2d1d
Paul <paulkluge@yahoo.de>
2024-09-23 11:33:17 +0200

    feature(group tags) – impemented basic group tags feature

--- src/sqlite_django-shell-commands/django-shell-examples.py
+++ src/sqlite_django-shell-commands/django-shell-examples.py
@@ -1,6 +1,6 @@
 # ✗ pipenv run python manage.py shell
 
-from product_feed_generator.models import Serverkast_Product, TopSystemsProduct
+from product_feed_generator.models import Product
 from product_feed_generator.models import FeedConfiguration, Feed
 
 Serverkast_Product.objects.all().delete()
@@ -11,3 +11,21 @@ FeedConfiguration.objects.create(
 feed_conf_from_current_shop_for_updating = FeedConfiguration.objects.filter(
     feed=feed_xyz
 )
+
+
+feed_conf_from_current_shop_for_updating = Feed.objects.filter(
+    shop_name="TopSystems"
+)
+print(feed_conf_from_current_shop_for_updating)
+
+
+#on the server
+# cd /home/datafeeds/production/feedfusion/src
+# % python3 -m pipenv run python manage.py shell
+from product_feed_generator.models import Product, Feed
+topsystemsfeed = Feed.objects.get(shop_name="TopSystems")
+feed_conf_from_current_shop_for_updating = Product.objects.filter(
+    feed=topsystemsfeed
+)
+print(feed_conf_from_current_shop_for_updating)
+feed_conf_from_current_shop_for_updating.update(is_selected=True)
